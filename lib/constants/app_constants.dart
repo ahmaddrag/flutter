c609fd530c409e8e69a259966a19ee5f03c5c4da
احمد خوب چهره <ahmadsaibar@gmail.com>
2022-08-20 21:30:41 +0430

    first commit

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,7 @@
+abstract class ThingsboardAppConstants {
+  static final thingsBoardApiEndpoint = 'http://2.181.234.100:8595';
+  static final thingsboardOAuth2CallbackUrlScheme = 'org.thingsboard.app.auth';
+
+  /// Not for production (only for debugging)
+  static final thingsboardOAuth2AppSecret = 'Your app secret here';
+}
